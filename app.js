e01b70ffae2296d3c214331a79ee0fd9bf59d40d
Odalia Senge <odalissenge05@outlook.com>
2025-06-06 15:09:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/admin', (req, res) => {
 });
 
 // Inicia el servidor
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
